467256f5c401daf56b81a3b20c14f6d5728c9d41
Frankie Hendley <frankiehendley@gmail.com>
2020-07-20 17:44:09 +0100

    drop down menu added for smaller screens

--- script.js
+++ script.js
@@ -10,16 +10,35 @@ portfolioItems.forEach(portfolioItem => {
     portfolioItem.childNodes[1].classList.remove('hover-darken');
   });
 });
-//sticky nav bar
-// window.onscroll = function() {myFunction()};
-//
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
-//
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+
+
+
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "nav-wrapper") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-wrapper";
+  }
+}
+
+
+
+
+function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
